d1436742154387822facf756e51bfb969155ab31
darshanqubits21 <darshanqubits@gmail.com>
2023-01-09 12:27:55 +0545

    creating local modules called add.js and importing in index.js with the help of require fn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("Index Page");
-// we are just importing the funcanality of add.js in index js
+// we are just importing the funcanality of add.js in index js with th help of require function
 //  In this case we can only use the funcanality and value defined in add.js but cannot control the value of add.js
 const fs = require("./add");
